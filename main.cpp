cbfda170ad58b91ca6780cabcc5eb45df13889eb
Kane Rodriguez <kaustez@siu.edu>
2016-03-26 00:03:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <string> // add an IFNDEF section to this file
+#include "Users.h"
+
 using namespace std;
 
 int main()
 {
-  
+	/*
+			this is just to show how you can make the administrator
+	*/
+	
+	
+	Admin administrator("A12345", "Nick Whetstone", "ratherB_a_wet_stone9696", "my name is nick");
 
+	administrator.displayUserData(&administrator);
 
+	/* 
 
+			this is meant to show you guys polymorphism 
 
+	
+	User firstUser("A12345", "Nick Whetstone", "ratherB_a_wet_stone9696", "my name is nick");
+	Admin firstAdmin; 
+	User *pfirstAdmin = &firstAdmin;
 
-  return 0;
+	*pfirstAdmin = firstUser;
+
+	cout << firstAdmin.itsID;
+
+*/
+
+	cin.get(); // pause screen
+
+	return 0;
 }
