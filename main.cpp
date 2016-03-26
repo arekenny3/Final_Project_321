1b8a4009121608421bc21a9de15ad68f4429fe4d
Kane Rodriguez <kaustez@siu.edu>
2016-03-26 18:24:58 -0500

    Update main.cpp
    added a quick tutorial into how the classes can be used. feel free to play!...

--- main.cpp
+++ main.cpp
@@ -7,28 +7,61 @@ using namespace std;
 int main()
 {
 	/*
-			this is just to show how you can make the administrator
+		this is just to show how you can make the administrator
 	*/
-	
-	
+
+
 	Admin administrator("A12345", "Nick Whetstone", "ratherB_a_wet_stone9696", "my name is nick");
 
 	administrator.displayUserData(&administrator);
 
-	/* 
+	/*
+		the administrator can change other users data
+	*/
 
-			this is meant to show you guys polymorphism 
+	administrator.changeUserID(&administrator, "A00005");
+	administrator.changeUserName(&administrator, "Kane Rodriguez");
+	administrator.changeUserUsername(&administrator, "kaustez");
+	administrator.changeUserPassword(&administrator, "whats a pointer");
 
-	
-	User firstUser("A12345", "Nick Whetstone", "ratherB_a_wet_stone9696", "my name is nick");
-	Admin firstAdmin; 
-	User *pfirstAdmin = &firstAdmin;
+	cout << '\n';
 
-	*pfirstAdmin = firstUser;
+	administrator.displayUserData(&administrator);
 
-	cout << firstAdmin.itsID;
 
-*/
+	/*
+		the administrator can create users
+	*/
+
+	Student firstStudent;
+	string id = "S12123";
+	string name = "Caleb N.";
+	string username = "youresocool";
+	string password = "i like cats";
+
+	administrator.createUser(&firstStudent, id, name, username, password);
+
+	cout << '\n';
+
+	administrator.displayUserData(&firstStudent);
+
+
+	/*
+		students and faculty can view their real name and username
+	*/
+
+	cout << '\n';
+
+	firstStudent.displayUserData(&firstStudent);
+
+	/*
+		but they cannot access the data from other users
+	*/
+
+	cout << '\n';
+
+	firstStudent.displayUserData(&administrator); // will display an error message
+
 
 	cin.get(); // pause screen
 
