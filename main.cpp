92c3d87d62dd73e2ef72ff1670d4d6e2e2f9897d
Kane Rodriguez <kaustez@siu.edu>
2016-04-10 15:26:58 -0500

    Update main.cpp
    Turned echo back on after user entered password, added a while loop that would display an error message if login was unsuccessful

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 
 //Protoyped functions
 void welcome();
-void UseAndPass();
+bool login(); // both login and loginVerification might not be 'safe' and login is now platform dependent to windows.. consider changing in future
+bool loginVerification(string,string);
 
 using namespace std;
 
@@ -15,9 +16,15 @@ int main()
 	//admin_tutorial(); // uncomment if you would like to see how the admin class works. Look at the code as well
 
 	welcome();
-	UseAndPass();
+	while (!login()) // was login successful?
+	{
+		cout << "\n\n\t--Error-- Incorrect Username or Password"; /* perhaps make separate function for error handling in main 
+																  (NOTE: maybe implement try-catch later on */
+	}
 
-	cin.get(); // pause screen
+	cin.clear();
+	cin.ignore();
+	cin.get();
 
 	return 0;
 }
@@ -27,10 +34,10 @@ void welcome()
 	cout << "\n\n\t\tWelcome to D2S (Desire To Schedule)";
 }
 
-void UseAndPass()
+bool login()
 {
 	string username;
-	string password ;
+	string password;
 
 	cout << "\n\n\tEnter a username: ";
 	cin >> username;
@@ -42,4 +49,26 @@ void UseAndPass()
 	SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
 	getline(cin, password);
 	cin >> password;
+
+
+	/* Must turn screen ouput back on for user input */
+	SetConsoleMode(hStdin, mode |= ENABLE_ECHO_INPUT);
+
+
+
+	/* 
+		NOTE: This code is dependent on WINDOWS. Consider changing in future OR find a
+			 way to only include this on windows, and have separate methods for implementing on
+			 Mac, Linux, etc. 
+	*/
+	return loginVerification(username, password);
+}
+
+bool loginVerification(string username, string password)
+{
+	// implement in future. For now: Return true
+	cout << "\n\n\tYour Username: " << username
+		<< "\n\n\tYour Password: " << password; // NOTE: For testing puposes only, delete later
+
+	return true;
 }
