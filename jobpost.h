11d33aa43053c0071487449cf25f892b73c6f0ec
nathantb <nathantb@siu.edu>
2016-04-13 16:44:04 -0500

    Create jobpost.h

---
+++ jobpost.h
@@ -0,0 +1,29 @@
+//Job post class
+
+/*
+
+Authorized faculty can add and edit job postings.
+Students can view.
+
+Admin can edit any job posting.
+Only faculty who create post can edit.
+
+
+
+jobpost:
+  Title
+  Description
+  
+  
+  Time Posted
+  Author (i.e. person hiring like faculty member)
+
+
+*/
+
+class jobpost
+{
+
+
+
+};
