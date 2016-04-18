65e2174aa266e387f92d3c4bd6d899801e013a31
Samuel Morris <shmolagin@gmail.com>
2016-04-17 19:51:54 -0500

    Update to classes.
    I still need to add some functions and we need to decide how to implement the calendar.

--- courses.h
+++ courses.h
@@ -1,11 +1,60 @@
-/* Needs to have Exam class with course, time and test description.
+#include <iostream>
+#include <string>
 
-*/
+using namespace std;
 
-class Exams
+class Exam
 {
   protected:
-  
+	int examID;
+	int examDay;
+	int examWeek;
+	int examHour;
+	int examClass;
+	string examDescription;
   public:
-  
+	Exam() {}
+	Exam(int, int, int, int, int, string);
+	
+	ind getExamID {return examID;}
+	int getDay {return examDay;}
+	int getWeek {return examWeek;}
+	int getHour {return examHour;}
+	int getClass {return examClass;}  
+	int getDescription {return examDescription;}
 };
+
+Exam::Exam(int ID, int day, int week, int hour, int class, string description)
+{
+	examID = ID;
+	examDay = day;
+	examWeek = week;
+	examHour = hour;
+	examClass = class;
+	examDescription = description;
+}
+
+class Course
+{
+protected:
+	int courseID;
+	string courseName;
+	string courseDescription;
+	string instructorID;
+public:
+	Course() {}
+	Course(int, string, string, string);
+
+	int getCourseID() {return courseID;}
+	string getName {return courseName;}
+	string getInstructor {return instructorID;}
+	string getDescription; {return courseDescription;}
+};
+
+Course::Course(int ID, string name, string description, string instructor)
+{
+	courseID = ID:
+	courseName = name;
+	courseDescription = description;
+	instructorID = instructor;
+}
