c6f66dbcf3f8d9a837f9356de9c458e5632be4cc
yoursocool90 <calebn9@siu.edu>
2016-04-18 13:21:29 -0500

    created outline for the main menu

--- d2s/main.cpp
+++ d2s/main.cpp
@@ -15,7 +15,8 @@ using namespace tinyxml2;
 void welcome();
 
 using namespace std;
-
+void test(User*);
+int fakeMenu();
 int main()
 {
 	//admin_tutorial(); // uncomment if you would like to see how the admin class works. Look at the code as well
@@ -23,22 +24,91 @@ int main()
 	welcome();
 
 	User* newUser = new User; // Memory Leak? I made news all the way through.
-	//newUser = getUser(); // go through the login and verification process. SHOULD NOT MOVE PAST THIS POINT IF THEY CAN'T LOGIN
+	newUser = getUser(); // go through the login and verification process. SHOULD NOT MOVE PAST THIS POINT IF THEY CAN'T LOGIN
 
 	// this is where calebs menu function will go --- void d2sMainMenu(string type);
 	
-	d2sDatabaseSystem();
-
+	//d2sDatabaseSystem();
+	test(newUser);
 	cin.clear();
 	delete newUser;
 	newUser = 0;
-
+	cin.clear();
+	cin.ignore();
 	return 0;
 }
 
 void welcome()
 {
 	cout << "\n\n\t\tWelcome to D2S (Desire To Schedule)";
+}
+
+void test(User* myUser)
+{
+	string s = userType(myUser);
+	int count = 1,
+		choice = 0;
+
+	bool exit = false;
+
+	databaseWelcomeMessage();
+
+	while (!exit)
+	{
+		while (!(choice = fakeMenu()) || (choice > 5 || choice < 1))
+		{
+			errorWarning("That Was Not One of the Options");
+		}
+		switch (choice)
+		{
+		case 1: //
+			break;
+		case 2: //
+			break;
+		case 3: //
+			break;
+		case 4: //
+			break;
+		case 5: exit = true;
+			break;
+		default: // do something
+			break;
+		}
+	}
+
+	if (s == "student" || 
+		s == "faculty")
+	{
+		cout << "\n\n\t\t" << count << ": Im an " << s;
+		++count;
+	}
+	if (s == "admin")
+	{
+		cout << "\n\n\t\t" << count << ": Im an " << s;
+		++count;
+	}
+	if (s == "faculty")
+	{
+		cout << "\n\n\t\t" << count << ": Im an " << s;
+		++count;
+	}
+}
+int fakeMenu()
+{
+	int x = 0;
+	cout << "\n\n\tDatabase Options:"
+		<< "\n\t1. Write to Database"
+		<< "\n\t2. Read from Database"
+		<< "\n\t3. Search Database"
+		<< "\n\t4. Remove from Database"
+		<< "\n\t5. Exit"
+		<< "\n\t: ";
+
+	scanf_s("%d", &x);
+
+	getchar();
+
+	return x;
 }
 
 /*
